ee9275336701859072123e653d096f105524606d
José Lazaretti <eduardo1793@gmail.com>
2024-05-15 20:08:25 -0300

    Using config to create the dbConnectionUri

--- src/main_api.ts
+++ src/main_api.ts
@@ -1,14 +1,17 @@
 import { GetAccountById } from './application/usecase/GetAccountById';
-import { AccountRepositoryPostgres } from './infra/repository/AccountRepository';
-import { SignUp } from './application/usecase/SignUp';
-import { ExpressHttpServerAdapter } from './infra/http/HttpServer';
-import { AccountController } from './infra/http/AccountController';
-import { PgPromiseAdapter } from './infra/database/DatabaseConnection';
-import { AuthController } from './infra/http/AuthController';
 import { SignIn } from './application/usecase/SignIn';
+import { SignUp } from './application/usecase/SignUp';
+import { PgPromiseAdapter } from './infra/database/DatabaseConnection';
+import { AccountController } from './infra/http/AccountController';
+import { AuthController } from './infra/http/AuthController';
+import { ExpressHttpServerAdapter } from './infra/http/HttpServer';
+import { AccountRepositoryPostgres } from './infra/repository/AccountRepository';
+import config from 'config';
+
+const dbConnectionUri = `postgresql://${config.get('db.user')}:${config.get('db.password')}@${config.get('db.host')}:${config.get('db.port')}/${config.get('db.database')}`;
 
 const httpServer = new ExpressHttpServerAdapter();
-const databaseConnection = new PgPromiseAdapter('postgres://postgres:123456@localhost:5432/app');
+const databaseConnection = new PgPromiseAdapter(dbConnectionUri);
 
 const accountRepository = new AccountRepositoryPostgres(databaseConnection);
 const getAccountById = new GetAccountById(accountRepository);
