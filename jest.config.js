10fa2dcf2dbcb28e81da09fc50af2edf360db219
José Lazaretti <eduardo1793@gmail.com>
2024-05-18 13:53:21 -0300

    Updating jest configs

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,15 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFilesAfterEnv: ['<rootDir>/test/jest.setup.ts'],
+  testTimeout: 60000,
+  collectCoverage: true,
+  coveragePathIgnorePatterns: ['/node_modules/', 'main_api.ts', 'Server.ts', '/src/infra/http', '/test/helpers'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
--- test/jest.setup.ts
+++ test/jest.setup.ts
@@ -1,7 +1,5 @@
 import { DatabaseTestContainer } from './helpers/DatabaseTestContainer';
 
-jest.setTimeout(60000);
-
 afterAll(async () => {
   await DatabaseTestContainer.getInstance().stop();
 });
