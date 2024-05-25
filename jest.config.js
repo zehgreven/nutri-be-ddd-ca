ce4ed855b4a25ac29c36c25ff3410406315a5b56
José Lazaretti <eduardo1793@gmail.com>
2024-05-24 21:24:44 -0300

    Adding paths

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,10 @@ module.exports = {
   testTimeout: 60000,
   collectCoverage: true,
   coveragePathIgnorePatterns: ['/node_modules/', 'main_api.ts', 'Server.ts', '/src/infra/http', '/test/helpers'],
+  moduleNameMapper: {
+    '@src/(.*)': '<rootDir>/src/$1',
+    '@test/(.*)': '<rootDir>/test/$1',
+  },
   coverageThreshold: {
     global: {
       branches: 97,
